73dfdced4072481d6abf68c5c4a4a4d2a26ba88a
ShreyashMishra10 <shreyashmishra9044@gmail.com>
2026-02-22 15:10:35 +0530

    Finalised the project by adding delete button, date input field and css

--- todo.js
+++ todo.js
@@ -5,8 +5,17 @@ const todoList = [];
 function render() {
   let html = "";
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const tHtml = `<p>${todo}</p>`;
+    const todoObject = todoList[i];
+    // const name= todoObject.name;
+    // const dueDate = todoObject.dueDate;
+    const {name, dueDate} = todoObject; 
+    const tHtml = `
+    <div>${name}</div>
+    <div>${dueDate }</div>
+    <button onclick="
+    todoList.splice(${i}, 1);
+    render();
+    " class="css-delete">Delete</button>`;
     html += tHtml;
   }
   document.querySelector(".js-tList").innerHTML = html;
@@ -14,7 +23,16 @@ function render() {
 function addTodo() {
   const inputElement = document.querySelector(".js-input");
   const name = inputElement.value;
-  todoList.push(name);
+  const dateElement = document.querySelector(".js-date");
+  const dueDate = dateElement.value;
+  
+  todoList.push({
+    // name : name,
+    name,
+    // dueDate : dueDate
+    dueDate
+  });
   inputElement.value = "";
+  dateElement.value = "";
   render();
 }
